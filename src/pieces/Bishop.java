d290906e6dee573a62fe57d35e9964d961624aa9
unknown <lci2020051@iiitl.ac.in>
2025-09-21 16:03:32 +0530

    Implementing Piece classes

---
+++ src/pieces/Bishop.java
@@ -0,0 +1,80 @@
+package pieces;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import board.Board;
+import board.Tile;
+import misc.Alliance;
+import board.BoardUtils;
+import board.Move;
+import board.Move.AttackMove;
+import board.Move.MajorMove;
+
+
+public class Bishop extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = { -9, -7, 7, 9 };
+
+    public Bishop(int piecePosition, Alliance pieceAlliance) {
+        super(PieceType.BISHOP,piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(final Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for(final int candidateCoordinateOffset: CANDIDATE_MOVE_VECTOR_COORDINATES) {
+
+            int candidateDestinationCoordinate = this.piecePosition;
+
+            while(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                if(isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset) ||
+                    isEightColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset)) {
+                        break;
+                    }
+
+                
+                candidateDestinationCoordinate += candidateCoordinateOffset;
+                if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                    if(!candidateDestinationTile.isTileOccupied()) {
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    } else {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+                        if(this.pieceAlliance != pieceAlliance) {
+                            legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+
+    @Override
+    public String toString(){
+        return PieceType.BISHOP.toString();
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == 7);
+    }
+
+    private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 9);
+    }
+
+    @Override
+    public Bishop movePiece(final Move move) {
+        return  new Bishop(move.getDestinationCoordinate(),move.getMovedPiece().getPieceAlliance());
+    }
+
+}
